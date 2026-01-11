8388f38c1f0fb96ca5c518dd480b27273abeefeb
Hyeon Lee <103764100+hyeonlee717@users.noreply.github.com>
2026-01-11 12:55:38 +0900

    first commit

---
+++ src/main/java/com/apple/shop/ShopApplication.java
@@ -0,0 +1,24 @@
+package com.apple.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopApplication.class, args);
+
+		var test = new Friend("lee");
+		System.out.println(test.name);
+	}
+
+}
+
+class Friend {
+	String name = "kim";
+	int age = 20;
+	Friend(String a){
+		this.name = a;
+	}
+}
