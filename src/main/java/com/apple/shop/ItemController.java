15575b561878c77375f55aaec589f856a1c300e1
Hyeon Lee <103764100+hyeonlee717@users.noreply.github.com>
2026-01-19 06:54:40 +0900

    DB에서 GET한 DATA의 ID를 상세페이지 URL에 parameter로 넣고 HTML에 DATA 입력하는 thymeleaf 문법, 예외처리 error.html

--- src/main/java/com/apple/shop/ItemController.java
+++ src/main/java/com/apple/shop/ItemController.java
@@ -4,9 +4,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
@@ -36,6 +38,21 @@ public class ItemController {
         item.setPrice(price);
         itemRepository.save(item);
         return "redirect:/list";
+    }
+
+    @GetMapping("/detail/{id}")
+    String detail(@PathVariable Long id, Model model) {
+
+        Optional<Item> result = itemRepository.findById(id);
+
+        if (result.isPresent()) {
+            System.out.println(result.get());
+            model.addAttribute("data", result.get());
+            return "detail.html";
+        } else {
+            return "redirect:/list";
+        }
+
     }
 
 }
